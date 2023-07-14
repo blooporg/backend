bee646ecc6e457cb9edcac9870e308fbf110f3e0
Bilal Khan <bilalkhanrecovered@gmail.com>
2023-07-14 11:36:44 +0500

    modified the files

--- handlers/home.go
+++ handlers/home.go
@@ -1,15 +1,14 @@
 package handlers
 
 import (
-	"log"
+	"fmt"
 	"net/http"
-
-	"github.com/ibilalkayy/Bloop/backend/templates"
 )
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
-	err := templates.HomeTmpl.Execute(w, nil)
-	if err != nil {
-		log.Fatal()
-	}
+	// err := templates.HomeTmpl.Execute(w, nil)
+	// if err != nil {
+	// 	log.Fatal()
+	// }
+	fmt.Fprintf(w, "<b>Home Page</b>")
 }
